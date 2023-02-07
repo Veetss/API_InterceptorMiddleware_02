bbe42fec868ba34614197601d437657944d6086c
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2023-02-07 14:07:47 +0100

    Update MonthInterceptor.java

--- src/main/java/co/develhope/interceptorsmiddleware2/interceptors/MonthInterceptor.java
+++ src/main/java/co/develhope/interceptorsmiddleware2/interceptors/MonthInterceptor.java
@@ -1,16 +1,21 @@
 package co.develhope.interceptorsmiddleware2.interceptors;
 
 import co.develhope.interceptorsmiddleware2.entities.Month;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class MonthInterceptor implements HandlerInterceptor {
 
-    private static final List<Month> MONTHS = Arrays.asList(
+    private static final List<Month> months = Arrays.asList(
             new Month(1, "January", "Gennaio", "Januar"),
             new Month(2, "February", "Febbraio", "Februar"),
             new Month(3, "March", "Marzo", "März"),
@@ -18,12 +23,28 @@ public class MonthInterceptor implements HandlerInterceptor {
             new Month(5, "May", "Maggio", "Mai"),
             new Month(6, "June", "Giugno", "Juni"));
 
-    public static Month getMonthByNumber(int monthNumber) {
-        for (Month month : MONTHS) {
-            if (month.getMonthNumber() == monthNumber) {
-                return month;
-            }
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String header = request.getHeader("monthNumber");
+        if (header == null || header.isEmpty()) {
+            response.sendError(HttpStatus.BAD_REQUEST.value(), "monthNumber header is missing");
+            return false;
         }
-        return new Month(0, "nope", "nope", "nope");
+
+        int monthNumber = Integer.parseInt(header);
+        Month month = months.stream().filter(m -> m.getMonthNumber() == monthNumber).findFirst().orElseGet(() -> new Month(0, "nope", "nope", "nope"));
+        request.setAttribute("month", month);
+
+        return true;
+
     }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+    }
+
 }
